8f05b8b2bf1a74be018f3319434087a3cffd5bff
ET <et1103@hotmail.com>
2017-11-19 14:48:56 -0800

    setup student reducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, createStore } from 'redux';
-import { StudentReducer } from './reducers/studentReducers'
+import StudentReducer from './reducers/studentReducers'
 
 const rootReducers = combineReducers({
     students: StudentReducer
