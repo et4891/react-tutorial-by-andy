7ff9475e6041e54612c6f90d4b3b33277ee4cb35
ET <et1103@hotmail.com>
2017-11-19 14:47:45 -0800

    setup student reducer

---
+++ src/reducers/studentReducers.js
@@ -0,0 +1,8 @@
+const StudentReducer = (state = 0, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default StudentReducer;
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { combineReducers, createStore } from 'redux';
+import { StudentReducer } from './reducers/studentReducers'
+
+const rootReducers = combineReducers({
+    students: StudentReducer
+});
+
+const store = createStore(rootReducers);
